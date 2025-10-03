6a0a727c410cd1b48e40bcb627612dd2132b22fc
fayaz777-fz <shaikfayaz0528@gmail.com>
2025-10-03 14:45:44 +0530

    Frontend

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Logo.css';
+import logoImage from '../assets/logo.jpeg';
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <div className="logo-content">
+      <img src={logoImage} alt="Vaahan Bazaar Logo" className="logo-image" />
+      <div className="logo-text">
         <span className="logo-highlight">Two</span>
         <span className="logo-normal">Wheeler</span>
         <span className="logo-pro">Pro</span>
